ecaa80169d5b1680f411993b766109a424f739c1
Georgios Fotopoulos <xorz57@gmail.com>
2026-09-16 23:31:52 +0200

    Add input attributes for normal and UV coordinates in vertex shader

--- example_102/assets/shaders/default.vert.glsl
+++ example_102/assets/shaders/default.vert.glsl
@@ -1,6 +1,8 @@
 #version 460 core
 
 layout (location = 0) in vec3 a_position;
+layout (location = 1) in vec3 a_normal;
+layout (location = 2) in vec3 a_uv;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
