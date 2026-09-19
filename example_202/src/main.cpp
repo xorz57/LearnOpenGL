b261aee676fbc1d80f776956ccb7e54da9e96ebe
Georgios Fotopoulos <xorz57@gmail.com>
2026-09-19 15:10:13 +0200

    Rename examples

--- example_202/src/main.cpp
+++ example_202/src/main.cpp
@@ -53,7 +53,7 @@ auto main() -> int {
 
   float const main_scale{SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay())};
 
-  char const *title{"example_102"};
+  char const *title{"example_202"};
   std::int32_t const window_w{static_cast<std::int32_t>(1'280 * main_scale)};
   std::int32_t const window_h{static_cast<std::int32_t>(720 * main_scale)};
   SDL_WindowFlags const flags{SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN |
@@ -131,7 +131,7 @@ auto main() -> int {
   ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
   ImGui_ImplOpenGL3_Init(glsl_version);
 
-  std::vector<Vertex> vertices{
+  std::vector<Vertex> const vertices{
       // Front (+z)
       {.position = {-0.5F, -0.5F, 0.5F}, .normal = {0.0F, 0.0F, 1.0F}, .uv = {0.0F, 0.0F}},
       {.position = {0.5F, -0.5F, 0.5F}, .normal = {0.0F, 0.0F, 1.0F}, .uv = {1.0F, 0.0F}},
@@ -164,7 +164,7 @@ auto main() -> int {
       {.position = {-0.5F, -0.5F, 0.5F}, .normal = {0.0F, -1.0F, 0.0F}, .uv = {0.0F, 1.0F}},
   };
 
-  std::vector<uint32_t> indices{
+  std::vector<uint32_t> const indices{
       0,  1,  2,  2,  3,  0,  // Front
       4,  5,  6,  6,  7,  4,  // Back
       8,  9,  10, 10, 11, 8,  // Left
@@ -173,36 +173,70 @@ auto main() -> int {
       20, 21, 22, 22, 23, 20, // Bottom
   };
 
+  struct InstanceData {
+    glm::mat4 model;
+    glm::vec3 color;
+  };
+
+  std::vector<InstanceData> const instances{
+      {.model = glm::translate(glm::mat4(1.0F), {-1.0F, 2.0F, -1.0F}), .color = {1.0F, 0.0F, 0.0F}},
+      {.model = glm::translate(glm::mat4(1.0F), {1.0F, 2.0F, 1.0F}), .color = {0.0F, 1.0F, 0.0F}},
+      {.model = glm::scale(glm::mat4(1.0F), {5.0F, 1.0F, 5.0F}), .color = {0.0F, 0.0F, 1.0F}},
+  };
+
   std::uint32_t vbo{};
   std::uint32_t ebo{};
+  std::uint32_t ibo{};
 
   std::uint32_t vao{};
 
   glCreateBuffers(1, &vbo);
   glCreateBuffers(1, &ebo);
+  glCreateBuffers(1, &ibo);
   auto vbo_cleanup{ScopeExit{[&]() -> void { glDeleteBuffers(1, &vbo); }}};
   auto ebo_cleanup{ScopeExit{[&]() -> void { glDeleteBuffers(1, &ebo); }}};
+  auto instance_buffer_cleanup{ScopeExit{[&]() -> void { glDeleteBuffers(1, &ibo); }}};
 
   glCreateVertexArrays(1, &vao);
   auto vao_cleanup{ScopeExit{[&]() -> void { glDeleteVertexArrays(1, &vao); }}};
 
   glNamedBufferStorage(vbo, static_cast<std::ptrdiff_t>(vertices.size() * sizeof(Vertex)), vertices.data(), 0);
   glNamedBufferStorage(ebo, static_cast<std::ptrdiff_t>(indices.size() * sizeof(std::uint32_t)), indices.data(), 0);
+  glNamedBufferStorage(ibo, static_cast<std::ptrdiff_t>(instances.size() * sizeof(InstanceData)), instances.data(), 0);
 
   glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(Vertex));
+  glVertexArrayVertexBuffer(vao, 1, ibo, 0, sizeof(InstanceData));
   glVertexArrayElementBuffer(vao, ebo);
 
   glEnableVertexArrayAttrib(vao, 0);
   glEnableVertexArrayAttrib(vao, 1);
   glEnableVertexArrayAttrib(vao, 2);
+  glEnableVertexArrayAttrib(vao, 3);
+  glEnableVertexArrayAttrib(vao, 4);
+  glEnableVertexArrayAttrib(vao, 5);
+  glEnableVertexArrayAttrib(vao, 6);
+  glEnableVertexArrayAttrib(vao, 7);
 
   glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, position));
   glVertexArrayAttribFormat(vao, 1, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, normal));
   glVertexArrayAttribFormat(vao, 2, 2, GL_FLOAT, GL_FALSE, offsetof(Vertex, uv));
+  glVertexArrayAttribFormat(vao, 3, 4, GL_FLOAT, GL_FALSE, offsetof(InstanceData, model) + (0 * sizeof(glm::vec4)));
+  glVertexArrayAttribFormat(vao, 4, 4, GL_FLOAT, GL_FALSE, offsetof(InstanceData, model) + (1 * sizeof(glm::vec4)));
+  glVertexArrayAttribFormat(vao, 5, 4, GL_FLOAT, GL_FALSE, offsetof(InstanceData, model) + (2 * sizeof(glm::vec4)));
+  glVertexArrayAttribFormat(vao, 6, 4, GL_FLOAT, GL_FALSE, offsetof(InstanceData, model) + (3 * sizeof(glm::vec4)));
+  glVertexArrayAttribFormat(vao, 7, 3, GL_FLOAT, GL_FALSE, offsetof(InstanceData, color));
+
+  // glVertexArrayBindingDivisor(vao, 0, 0);
+  glVertexArrayBindingDivisor(vao, 1, 1);
 
   // glVertexArrayAttribBinding(vao, 0, 0);
   // glVertexArrayAttribBinding(vao, 1, 0);
   // glVertexArrayAttribBinding(vao, 2, 0);
+  glVertexArrayAttribBinding(vao, 3, 1);
+  glVertexArrayAttribBinding(vao, 4, 1);
+  glVertexArrayAttribBinding(vao, 5, 1);
+  glVertexArrayAttribBinding(vao, 6, 1);
+  glVertexArrayAttribBinding(vao, 7, 1);
 
   std::optional<Shader> shader{
       Shader::loadFromFile(ASSETS_DIR "shaders/unlit_color.vert.glsl", ASSETS_DIR "shaders/unlit_color.frag.glsl")};
@@ -301,35 +335,12 @@ auto main() -> int {
     shader->setUniform("u_projection", projection);
     shader->setUniform("u_view", view);
 
-    glm::mat4 model1{1.0F};
-    model1 = glm::translate(model1, {-1.0F, 2.0F, -1.0F});
-    shader->setUniform("u_model", model1);
-    shader->setUniform("u_color", glm::vec3{1.0F, 0.0F, 0.0F});
     glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES,
-                   static_cast<std::int32_t>(indices.size()),
-                   GL_UNSIGNED_INT,
-                   static_cast<void *>(nullptr));
-
-    glm::mat4 model2{1.0F};
-    model2 = glm::translate(model2, {1.0F, 2.0F, 1.0F});
-    shader->setUniform("u_model", model2);
-    shader->setUniform("u_color", glm::vec3{0.0F, 1.0F, 0.0F});
-    glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES,
-                   static_cast<std::int32_t>(indices.size()),
-                   GL_UNSIGNED_INT,
-                   static_cast<void *>(nullptr));
-
-    glm::mat4 model3{1.0F};
-    model3 = glm::scale(model3, {5.0F, 1.0F, 5.0F});
-    shader->setUniform("u_model", model3);
-    shader->setUniform("u_color", glm::vec3{0.0F, 0.0F, 1.0F});
-    glBindVertexArray(vao);
-    glDrawElements(GL_TRIANGLES,
-                   static_cast<std::int32_t>(indices.size()),
-                   GL_UNSIGNED_INT,
-                   static_cast<void *>(nullptr));
+    glDrawElementsInstanced(GL_TRIANGLES,
+                            static_cast<std::int32_t>(indices.size()),
+                            GL_UNSIGNED_INT,
+                            static_cast<void *>(nullptr),
+                            static_cast<std::int32_t>(instances.size()));
 
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
